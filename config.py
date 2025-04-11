3c8e320a9199f3cdc8ee2f4154fcfdd6fe9c748b
Abhishek Shah <abhishekshah3@gmail.com>
2025-04-11 17:23:10 +0530

    fix cli args - supply default vals

--- config.py
+++ config.py
@@ -13,13 +13,17 @@ class Config:
     clean: bool = False
     chainspec: str = "dev"
     bin: str = "substrate"
-    account_key_type: AccountKeyType = "ecdsa"
+    account_key_type: AccountKeyType = AccountKeyType.AccountId20
 
 
 def parse_args() -> Config:
     parser = argparse.ArgumentParser(description="PySubnet launcher")
     parser.add_argument(
-        "-i", "--interactive", action="store_true", help="Run in interactive mode"
+        "-i",
+        "--interactive",
+        action="store_true",
+        default=False,
+        help="Run in interactive mode",
     )
     parser.add_argument(
         "--run",
@@ -27,6 +31,7 @@ def parse_args() -> Config:
         "-r",
         dest="run_network",
         action="store_true",
+        default=False,
         help="Run the network after generating keys and chainspecs",
     )
     parser.add_argument(
@@ -36,10 +41,14 @@ def parse_args() -> Config:
         "-c",
         "--clean",
         action="store_true",
+        default=False,
         help="Clean the root directory before starting",
     )
     parser.add_argument(
-        "--chainspec", type=str, help="Path to chainspec file or 'dev' | 'local'"
+        "--chainspec",
+        type=str,
+        default="dev",
+        help="Path to chainspec file or 'dev' | 'local'",
     )
     parser.add_argument(
         "--bin", type=str, default="substrate", help="Path to substrate binary"
@@ -48,16 +57,20 @@ def parse_args() -> Config:
         "--account",
         type=AccountKeyType.from_string,
         choices=list(AccountKeyType),
+        default=AccountKeyType.AccountId20,
         help="Type of account key ('ecdsa' for Ethereum-style, 'sr25519' for Substrate-style)",
     )
 
     args = parser.parse_args()
+    # !! WARNING: argsparse will actually set non-supplied flags to None! This works for boolean values but
+    # for others it its a very annoying runtime error! Hence use + <default value> unless a default is provided
+    # in argsparse itself. We explicitly specify defaults for argparse itself so `or <default_val>` not required here
     return Config(
         interactive=args.interactive,
         run_network=args.run_network,
         root_dir=os.path.abspath(args.root),
         clean=args.clean,
-        chainspec=args.chainspec or "dev",
+        chainspec=args.chainspec,
         bin=os.path.abspath(args.bin),
-        account_key_type=args.account or AccountKeyType("ecdsa"),
+        account_key_type=args.account
     )
